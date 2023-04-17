81d91c9753858889f5ac914d79802fdf6e9e6a4f
albabernal03 <ALBABERO@MYUAX.COM>
2023-04-17 17:38:27 +0200

    en primer lugar importamos Pool de la libreria multiprocessing

---
+++ prueba.py
@@ -0,0 +1,4 @@
+from multiprocessing import Pool
+
+#Simulamos una gasolinera con n surtidores. Cuando llega un coche, se le asigna un surtidor y se le asigna un tiempo de llenado de la gasolina.
+class Coche:
